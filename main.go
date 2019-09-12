6e51059c2f869397c24795b3d62c5a3f94957c90
knight <viease@foxmail.com>
2019-09-12 14:35:08 +0800

    go fmt

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 
 	<-quit
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown:", err)
--- util/util.go
+++ util/util.go
@@ -1,8 +1,8 @@
 package util
 
 import (
-	"time"
 	"strconv"
+	"time"
 )
 
 func ParseInt(value string) int {
